381bf0b65186cb58dd2d4a02b7e0e190b9380752
Bearmino <minho51y@gmail.com>
2022-10-26 14:23:35 +0900

    Start python

---
+++ string.py
@@ -0,0 +1,25 @@
+#문자열로 표현
+
+print("Hello world")
+print('Hello world')
+print("""Phython is fun""")
+print('''Phython is Life''')
+
+#문자열에 작은 따옴표 포함 시키기
+food = "Python's favorite food"
+print(food)
+
+#문자열에 큰 따옴표 포함 시키기
+say = '"Phython is very easy".he say'
+print(say)
+
+#백슬러시(\)를 사용해서 작은,큰 따옴표 포함 시키기
+tell = "Python \'s very simple"
+print(tell)
+
+#여러 줄인 문자열을 변수에 대입하고 싶을 때
+multiLine = """
+Life is too short
+You need Python
+"""
+print(multiLine)
