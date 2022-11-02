841f11847b741aaa889f30213267d7fb439a26ab
Bearmino <minho51y@gmail.com>
2022-11-02 17:14:58 +0900

    While

--- While.py
+++ While.py
@@ -37,3 +37,23 @@ while money:
     if coffe == 0:
         print("커피가 다 떨어져서, 판매를 중지합니다.")
     break
+'''해당 소스는 money가 300으로 고정되어 있으므로, 항상 참으로 끝의 break가 없으면 무한 루프가 진행된다.'''
+
+'''IDLE에서의 실제 실습 환경 반영해보기'''
+#coffee.py
+
+coffee = 10
+while True
+    money = int(input("돈을 넣어주세요:"))
+    if money == 300:
+        print("커피를 줍니다.")
+        coffee = coffee -1
+    elif money < 300:
+        print("잔돈 %d를 주고 커피를 줍니다." % (money-300))
+        coffee = coffee -1
+    else:
+        print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+        print("남은 커피의 양은 %d개 입니다."% coffee)
+    if coffee == 0:
+        print("커피가 다 떨어졌습니다.판매를 중지합니다.")
+    break
