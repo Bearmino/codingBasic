95e599f2aebfdfef7e060f964ed082131f471447
Bearmino <minho51y@gmail.com>
2022-11-03 11:16:59 +0900

    For

--- For.py
+++ For.py
@@ -27,6 +27,6 @@ number=0
 for mark in marks:
     number+=1
     if mark >=60:
-        print("%d번 학생은 합격입니다" % number)
+        print("%d번 학생은 합격 입니다" % number)
     else:
-        print("%d번 학생은 불합격입니다."% number)
+        print("%d번 학생은 불합격 입니다."% number)
