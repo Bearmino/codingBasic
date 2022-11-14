3a32a5be169da87daa5a1cc829f032938c087c1b
Bearmino <minho51y@gmail.com>
2022-11-14 09:52:37 +0900

    python

---
+++ Chapter2/input.py
@@ -0,0 +1,11 @@
+#사용자 입력(input)
+"""사용자가 입력한 값을 어떤 변수에 대입하고자 할 때 """
+a=input("Life is too short, you need python")
+print(a)
+
+#프롬프트를 띄워서 사용자 입력받기
+number = input("숫자를 입력하세요: ")
+type(number)
+print(number)
+'''input은 모든 것을 문자열로 처리하기에, 실제로 숫자가 아님에 주의하자.'''
+
