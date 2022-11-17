51e24d124e7254c8f2fba0460cb42ffd956c2649
Bearmino <minho51y@gmail.com>
2022-11-17 11:43:57 +0900

    python

--- Chapter3/class.py
+++ Chapter3/class.py
@@ -75,12 +75,44 @@ class FourCal:
     def setdata(self,first,second):
         self.first = first
         self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+"""파이썬 메서드의 첫 번째 매개변수 이름은 관례적으로 self를 사용한다."""
 
 a=FourCal()
 a.setdata(4,2)
+"""setdata메서드의 첫번째 매개변수 self에는 setdata메서드를 호출한 객체 a가 자동으로 전달된다."""
 
 print(a.first,a.second)
+"""a → self , 4 → first, 2 → second, setdata 메서드의 매개변수 first,second에 4,2가 전달"""
 
+b=FourCal()
+b.setdata(3,7)
+print(b.first)
+""" FourCal클래스의 인스턴스인 b라는 객체를 새롭게 생성 후 setdata를 이용하여 매개변수 3,7전달"""
+print(b.first == a.first)
+# 해당 수식을 이용하여 확인해 보면 a 객체의 first값과 b 객체의 first값에 영향을 받지 않는다.
 
+# 더하기 기능 만들기
+    # def add(self):
+    #     result = self.first + self.second
+    #     return result
+"""add 메서드의 매개변수는 self이고, 반환값은 result이다."""
+print(a.add())
+"""a.add()와 같이 a 객체에 의해 add메서드가 수행되면서 add메서드의 self에는 객체 a가 자동으로 입력됨 """
+print(a.mul())
+print(a.sub())
+print(a.div())
+print(a.mul())
 
 
