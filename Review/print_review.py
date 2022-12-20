bf203de2ad04f7ecaedf4007371cd8fc8b89346c
Bearmino <minho51y@gmail.com>
2022-12-20 15:15:43 +0900

    python

---
+++ Review/print_review.py
@@ -0,0 +1,30 @@
+# 1. 화면에 Hello World 문자열을 출력하세요
+print('Hello World')
+
+# 2. 화면에 'Mary's cosmetics를 출력하세요.(중간에 '가 있음에 주의하세요)
+print("Mary's cosmetics")
+
+# 3. 화면에 신씨가 소리질렀다. "도둑이야". (중간에 "가 있음에 주의하세요)
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 4. 화면에 "C:\Windows"를 출력하세요.
+print("C:\Windows")
+
+# 5. 하기 코드를 실행하여 보고 \t와 \n의 역할을 설명하세요
+print("안녕하세요.\n만나서\t반갑습니다.")
+# \t는 탭을 의미하고, \n은 줄바꿈을 의미한다.
+
+# 6. "오늘은" "일요일"의 각각의 단어상태로 연결해서 출력하세요.
+print("오늘은","일요일")
+
+# 7. print()함수!!를 naver/kakao/samsung/sk 처럼 출력하세요.
+print("naver","kakao","samsung","sk", sep="/")
+
+# 8. print()함수를 사용하여 naver;kakao;samsung;sk 처럼 출력하세요.
+print("naver","kakao","samsung","sk", sep=";")
+
+# 9. 다음 코드를 수정하여 줄바꿈이 없이 출력하세요.(세미콜론은 한줄의 여러 개의 명령을 작성하기 위해 사용)
+# print("First");print("Second")
+print("First",end=" ");print("Second")
+
+# 10.
