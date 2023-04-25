d670e7d4fe4a61d138f790edb0cb36dae4e48f69
Bearmin <minho51y@gmail.com>
2023-04-25 14:30:03 +0900

    python

--- JumptoPython_library/textwrap_test.py
+++ JumptoPython_library/textwrap_test.py
@@ -10,9 +10,16 @@ print(shor_text_place) # Life is...중략..., placeholder 인자를 설정하면
 
 #textwrap.wrap()
 # textwrap.wrap()은 긴 문자열을 원하는 길이로 줄 바꿈(wrapping)할 때 사용하는 함수이다.
-
 long_text = 'Winners embrace hard work. They love the discipline of it, the trade-off theyre making to win. ' \
             'Losers, on the other hand, see it as punishment. And that’s the difference.'
-shor_text = textwrap.wrap(long_text, width=50)
-print(shor_text)
-print('\n'.join(shor_text))
+result_text = textwrap.wrap(long_text, width=50)
+print(result_text)
+# ['Winners embrace hard work. They love the', 'discipline of it, the trade-off theyre making to', 'win. Losers, on the
+#  other hand, see it as', 'punishment. And that’s the difference.']
+# textwrap.wrap() 함수는 긴 문자열을 width 길이만큼 자르고 이를 리스트로 만들어 반환함
+print('\n'.join(result_text))
+# Winners embrace hard work. They love the
+# discipline of it, the trade-off theyre making to
+# win. Losers, on the other hand, see it as
+# punishment. And that’s the difference.
+# join() 함수로 문자열 사이에 줄 바꿈 문자(\n)를 넣어 하나로 합친 다음 출력한다.
